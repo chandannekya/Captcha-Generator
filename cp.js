556b2dd36dbec53bea0c1ee7df71f19ddd750312
Chandan <chadan85338818@gmail.com>
2023-11-12 11:16:32 +0530

    new change

--- cp.js
+++ cp.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+
     const capscSc = document.querySelector(".capsc.sc input");
     const refreshButton = document.querySelector(".refresh");
     const submitButton = document.querySelector(".capsc.button button");
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const generateCaptcha = () => {
         const randomString = Math.random().toString(36).substring(2, 7);
         const randomStringArray = randomString.split("");
-        const changeString = randomStringArray.map((char) =>
+        const changeString = randomStringArray.map((char) => 
             Math.random() > 0.5 ? char.toUpperCase() : char
         );
         captcha = changeString.join("   ");
@@ -26,7 +26,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const captchaKeyUpValidate = () => {
         const captchaText = captcha.replace(/\s/g, "");
-        submitButton.disabled = captchaInputBox.value.trim() !== captchaText;
+        captchaInputBox.value.trim() !== captchaText;
+        
 
         if (!captchaInputBox.value) message.classList.remove("active");
     };
@@ -38,15 +39,26 @@ document.addEventListener("DOMContentLoaded", () => {
         if (captchaInputBox.value === captchaText) {
             message.innerText = "Entered captcha is correct";
             message.style.color = "#222620";
+            submitButton.disabled = false; // Enable the button if captcha is correct
         } else {
+            
+            refreshBtnClick();
+            message.classList.add("active");
             message.innerText = "Entered captcha is not correct";
             message.style.color = "#FF2525";
+            
+           
+
         }
     };
-
-    refreshButton.addEventListener("click", refreshBtnClick);
+    
+    // Call captchaKeyUpValidate on every keyup event within the captcha input field
     captchaInputBox.addEventListener("keyup", captchaKeyUpValidate);
+
+    // Call submitBtnClick when the submit button is clicked
     submitButton.addEventListener("click", submitBtnClick);
 
+    refreshButton.addEventListener("click", refreshBtnClick);
+
     generateCaptcha();
-});
+
